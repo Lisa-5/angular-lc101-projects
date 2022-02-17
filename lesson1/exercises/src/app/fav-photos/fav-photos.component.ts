cb48da05947853704fde56ff8c554110eb3974a3
Lisa Ulricksen <LisaHome@lisas-imac.attlocal.net>
2022-02-17 16:11:43 -0600

    saving after finishing Random Images Section

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Random Images';
+  image1 = 'https://images2.minutemediacdn.com/image/upload/c_crop,h_1351,w_2400,x_0,y_1049/f_auto,q_auto,w_1100/v1618241852/shape/mentalfloss/94532-gettyimages-1199037273.jpg';
+  image2 = 'https://i.natgeofe.com/n/57f411cd-dfe6-41a6-8d40-21ba583eccfb/lake-elsinore-california.jpg';
+  image3 = 'https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2013/03/butterfly_gardens_20090219_085.jpg?w=600&ssl=1';
 
   constructor() { }
 
